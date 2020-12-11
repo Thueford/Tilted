fd28a316f8254d9086463d5e987fb385155e8fd4
afeilke1 <afeilke1@hs-mittweida.de>
2020-12-11 22:49:12 +0100

    Update Spawner.cs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,28 +1,37 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject pfHuman;
-    private ArrayList humans = new ArrayList();
+    private List<GameObject> humans = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject o1 = Instantiate(pfHuman, new Vector3(300, 200, 0), Quaternion.identity);
+        RectTransform rt = gameObject.GetComponent<RectTransform>();
+        //rt.rect.width = 50f;
+        //rt.rect.height = 100f;
+        rt.localScale = new Vector3(50, 90, 0);
         humans.Add(o1);
     }
 
-    public ArrayList getHumans()
+    public GameObject[] getHumans()
     {
-        return humans;
+        return humans.ToArray();
     }
 
-    public void killHuman(GameObject o)
+    public void killHumans(params GameObject[] l)
     {
-        humans.Remove(o);
+        foreach(GameObject o in l) {
+            humans.Remove(o);
+            Destroy(o);
+        }
+
     }
 
     // Update is called once per frame
