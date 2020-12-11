b0b6f84127049263eae3bfc167697f69271b9c2c
lwutke <lwutke@hs-mittweida.de>
2020-12-11 23:47:20 +0100

    Update eart physicsc a little bit, but not much

--- Assets/Scripts/earth_physics.cs
+++ Assets/Scripts/earth_physics.cs
@@ -1,9 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class earth_physics : MonoBehaviour
 {
+
+    private Spawner s;
+
+    private double neigung;
+
+    private const float max_neigung = 30f;
+    private const float neutralizer = 385.3f;
+
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,12 @@ public class earth_physics : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        neigung = 0;
+        foreach(GameObject g in s.getHumans())
+        {
+            neigung += (g.transform.position.x - neutralizer);
+        }
+        float tmp = (float)(Math.Asin(neigung / (2 * 560)));
+        rb.rotation = tmp; 
     }
 }
