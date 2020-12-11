5b8ecc5c8099abb7e3ec6793a428a3537a3ca1d4
lwutke <lwutke@hs-mittweida.de>
2020-12-11 20:29:35 +0100

    added pause menu in GameScene

---
+++ Assets/Scripts/pause.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pause : MonoBehaviour
+{
+    public Image panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel = GetComponent<Image>();
+        panel.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void enterPause()
+    {
+        panel.gameObject.SetActive(true);
+    }
+
+    public void endPause()
+    {
+        panel.gameObject.SetActive(false);
+    }
+}
