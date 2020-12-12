24934d1bcb47d4f5addd50afb1392e2a6ea5098f
lwutke <lwutke@hs-mittweida.de>
2020-12-12 21:18:37 +0100

    Human-o-meter

---
+++ Assets/Scripts/Humanometer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Humanometer : MonoBehaviour
+{
+    public Text kt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        kt = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        kt.text = "Humans: " + Spawner.getHumans().Length;
+    }
+}
