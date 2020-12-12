96dac01d827549ca1fe0462dfaf8a6ccd038f897
afeilke1 <afeilke1@hs-mittweida.de>
2020-12-12 03:16:47 +0100

    Added spawn waves

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,36 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject pfHuman = null;
+    private static GameObject earth;
     private static List<GameObject> humans = new List<GameObject>();
-    private static Timer itvWave;
     private static System.Random rand = new System.Random();
 
     public static int wavePeriod = 1024;
     public static int waveVary = 512;
-     
+    public static int waveAmount = 5;
+    GameObject o;
+
     // Start is called before the first frame update
     void Start()
     {
-        //UnityEngine.Debug.Log("Hello");
-        itvWave = new Timer((time) => {
-            //UnityEngine.Debug.Log("World");
-            //spawnHuman((float)rand.NextDouble()*50 + 300, (float)rand.NextDouble()*50 + 200);
-            //int ran = (int)(waveVary * rand.NextDouble()) + wavePeriod;
-            //itvWave.Change(ran, ran);
+        
+        GameObject[] tmp = GameObject.FindGameObjectsWithTag("Earth");
+        if (tmp.Length == 0) throw new NullReferenceException("Earth not found.");
+        earth = tmp[0];
 
-            spawnHuman(300, 200);
-        }, null, 1000, 1000);
-
-        spawnHuman(300, 230);
-        spawnHuman(380, 250);
-        spawnHuman(450, 200);
+        StartCoroutine(spawnWave());
     }
 
     public static GameObject[] getHumans()
@@ -52,8 +45,26 @@ public class Spawner : MonoBehaviour
         
     }
 
+    private IEnumerator spawnWave()
+    {
+        yield return new WaitForSeconds(1);
+        while (true)
+        {
+            UnityEngine.Debug.Log("Spawn " + waveAmount + "Humans");
+            for (int i = 0; i < waveAmount; i++)
+            {
+                spawnHuman(
+                    earth.transform.position.x/2 + (float)rand.NextDouble() * earth.transform.position.x,
+                    earth.transform.position.y + 200 + (float)rand.NextDouble() * 400);
+            }
+
+            yield return new WaitForSeconds(4);
+        }
+    }
+
     private void spawnHuman(float x, float y)
     {
+        // UnityEngine.Debug.Log("Spawn Human at " + x + ", " + y);
         GameObject o = Instantiate(pfHuman, new Vector3(x, y, 0), Quaternion.identity);
         o.transform.localScale = new Vector3(0.25f, 0.4f, 0);
         humans.Add(o);
