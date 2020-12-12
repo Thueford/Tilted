ea03454488ba3aa27d2f7b73631c8958225b60ad
jmassals <jmassals@hs-mittweida.de>
2020-12-12 18:56:16 +0100

    sounds on pause

--- Assets/Scripts/pause.cs
+++ Assets/Scripts/pause.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class pause : MonoBehaviour
 {
     public Image panel;
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +19,25 @@ public class pause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void enterPause()
     {
         panel.gameObject.SetActive(true);
+        playClickSound();
         Time.timeScale = 0;
     }
 
     public void endPause()
     {
+        playClickSound();
         panel.gameObject.SetActive(false);
         Time.timeScale = 1;
+    }
+
+    private void playClickSound() {
+        audioSource.PlayOneShot(audioClips[new System.Random().Next(0, audioClips.Length)], 1f);
+        Debug.Log("Player Click");
     }
 }
