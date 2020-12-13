31c075ceb9faef25dab84b4fad3cbaf71b112600
lwutke <lwutke@hs-mittweida.de>
2020-12-13 12:11:17 +0100

    different stuff

--- Assets/Scripts/walking.cs
+++ Assets/Scripts/walking.cs
@@ -80,20 +80,19 @@ public class walking : MonoBehaviour
         /*winkel = GameObject.FindGameObjectsWithTag("Earth")[0].GetComponent<Rigidbody2D>().rotation;
 		
         Debug.Log("Winkel: " + winkel);*/
-        //if (!Bergsteiger && !climb && (rand.NextDouble() * 100) < Math.Abs(epEarth.cAngle) && last_collision < 0)
-        //    right = epEarth.cAngle < 0;
+        if (!Bergsteiger && !climb && (rand.NextDouble() * 100) < Math.Abs(epEarth.cAngle) && last_collision < 0)
+            right = epEarth.cAngle < 0;
 
-        //if (climb) right = epEarth.cAngle > 0;
-        //last_collision -= 0.5f;
+        if (climb) right = epEarth.cAngle > 0;
 
-        float winkel = GameObject.FindGameObjectsWithTag("Earth")[0].GetComponent<earth_physics>().cAngle;
+        /*float winkel = GameObject.FindGameObjectsWithTag("Earth")[0].GetComponent<earth_physics>().cAngle;
         if (!Bergsteiger && !climb && (rand.NextDouble() * 100) < Math.Abs(winkel))
         {
             if (last_collision < 0) right = winkel < 0;
             else if (winkel > 0) right = !right;
         }
 
-        if (climb) right = winkel > 0;
+        if (climb) right = winkel > 0;*/
         last_collision -= 0.3f;
     }
     public void moveY(float y)
