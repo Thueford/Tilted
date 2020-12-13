fb10beeec932e49a9ccfb1fc09ae64d5cac85a86
jmassals <jmassals@hs-mittweida.de>
2020-12-13 12:15:28 +0100

    added skill menu klicksounds

--- Assets/Scripts/Controls/openControls.cs
+++ Assets/Scripts/Controls/openControls.cs
@@ -6,21 +6,28 @@ using UnityEngine.SceneManagement;
 
 public class openControls : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void btn_pressed()
     {
         PlayerPrefs.SetInt("previousLevel", Application.loadedLevel);
+        playClickSound();
         SceneManager.LoadScene("ShowControlsScene");
+    }
+
+    private void playClickSound() {
+        audioSource.PlayOneShot(audioClips[new System.Random().Next(0, audioClips.Length)], 1f);
     }
 }
